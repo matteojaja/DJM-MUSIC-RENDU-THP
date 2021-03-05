a5a6e29a1af6b60b8e8c17331f2a52418a6474cd
matteojaja <44059219+matteojaja@users.noreply.github.com>
2021-03-05 10:18:05 +0100

    Add files via upload

---
+++ js/active.js
@@ -0,0 +1,10 @@
+
+//======>  JS POUR LE MENU SUR MOBILE
+
+$('.main-navigation').meanmenu({
+    meanScreenWidth: "992",
+    meanMenuContainer: '.mobile-menu',
+    meanMenuClose: "<i class='icon icon-simple-remove'></i>",
+    meanMenuOpen: "<i class='icon icon-menu-34'></i>",
+    meanExpand: "",
+});
